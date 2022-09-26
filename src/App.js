4637a262881d1e70a782c199824248b95d484561
olivia pasion <oap9820@gmail.com>
2022-09-26 14:25:10 -0700

    created .env, added App.test.js, adding route elements to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import BookList from './components/book/BookList';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Router>
+        <Switch>
+          <Route>
+            <BookList />
+          </Route>
+        </Switch>
+      </Router>
     </main>
   );
 }
