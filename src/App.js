c1480636d08b18d300c881ed3a700f028cd0fecf
olivia pasion <oap9820@gmail.com>
2022-09-27 08:52:08 -0700

    needs work still

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -12,8 +13,11 @@ function App() {
       <h1>Library Catalog</h1>
       <Router>
         <Switch>
-          <Route>
+          <Route exact path="/books">
             <BookList />
+          </Route>
+          <Route exact path="/books/:id">
+            <BookDetail />
           </Route>
         </Switch>
       </Router>
